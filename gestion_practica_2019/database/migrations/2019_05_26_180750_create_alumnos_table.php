09624b458c9ac89b75334a8091b674fa8a71311b
Eric Felipe Riveros Novoa <eric.thno@gmail.com>
2019-05-26 20:23:40 -0400

    Migrations
    Alumnos - Supervisores - Administradores - Practicas

---
+++ gestion_practica_2019/database/migrations/2019_05_26_180750_create_alumnos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('rut');
+            $table->string('email');
+            $table->string('direccion');
+            $table->string('fono');
+            $table->string('anno_ingreso');
+            $table->string('carrera');
+            $table->string('estimacion_semestre');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+}
---
+++ gestion_practica_2019/database/migrations/2019_05_26_181547_create_supervisores_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSupervisoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supervisores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('cargo');
+            $table->string('departamento');
+            $table->string('email');
+            $table->string('fono');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supervisores');
+    }
+}
