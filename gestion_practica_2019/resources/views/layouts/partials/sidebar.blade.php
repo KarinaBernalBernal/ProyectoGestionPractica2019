53b77e121cf08a5b5c802cff3f78b4b24be8012e
Eric Felipe Riveros Novoa <eric.thno@gmail.com>
2019-06-07 19:27:35 -0400

    mod

--- gestion_practica_2019/resources/views/layouts/partials/sidebar.blade.php
+++ gestion_practica_2019/resources/views/layouts/partials/sidebar.blade.php
@@ -31,13 +31,16 @@
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-users"></i>
-        <span> Gestión de cuentas y usuarios</span>
+        <span>Mantenedores</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Ing. Ejec. Informatica:</h6>
+            <a class="collapse-item" href="#">Alumnos</a>
+            <a class="collapse-item" href="#">Autoevaluaciones</a>
+            <h6 class="collapse-header">Ing. Civil Informatica:</h6>
+            <a class="collapse-item" href="#">Alumnos</a>
+            <a class="collapse-item" href="#">Autoevaluaciones</a>
         </div>
         </div>
     </li>
