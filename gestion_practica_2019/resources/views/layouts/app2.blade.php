d50d5781617f2bbdba8baa8b7097d560da259dda
Eric Felipe Riveros Novoa <eric.thno@gmail.com>
2019-06-08 00:06:32 -0400

    Mejoras
    * Cambio en las rutas de css, js, etc. en head y footer
    * Se agrego el tema a 'restablecer contraseña'

---
+++ gestion_practica_2019/resources/views/layouts/app2.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    @include('layouts.partials.head')
+    
+    <body class="bg-gradient-primary">
+        @yield('content')
+        @include('layouts.partials.footer-scripts')
+    </body>
+</html>
