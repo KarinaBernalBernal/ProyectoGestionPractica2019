92ccb75db28ebd3c5a376f93f546bf4205f01b45
Pedro Morales <pedro.ignaciom95@gmail.com>
2019-08-02 00:55:44 -0400

    cambios

---
+++ gestion_practica_2019/app/Autoevaluacion.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SGPP;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Autoevaluacion extends Model
+{
+    protected $primaryKey = 'id_autoeval';
+    protected $table = 'autoevaluaciones';
+
+    protected $fillable = [
+       	'f_entrega','id_practica'
+    ];
+
+    public function practica(){
+ 		return $this->belongsTo('App\User');
+    }
+    public function desempenno(){
+ 		return $this->hasOne('App\Desempenno');
+    }
+    public function tarea(){
+ 		return $this->belongsTo('App\Tarea');
+    }
+    public function habilidad(){
+ 		return $this->belongsTo('App\Habilidad');
+    }
+    public function conocimiento(){
+ 		return $this->belongsTo('App\Conocimiento');
+    }
+    public function herramientaPractica(){
+ 		return $this->belongsTo('App\HerramientaPractica');
+    }
+    public function areaAutoeval(){
+ 		return $this->belongsTo('App\AreaAutoeval');
+    }
+    public function evalActPractica(){
+ 		return $this->belongsTo('App\EvalActPractica');
+    }
+    public function evalConPractica(){
+ 		return $this->belongsTo('App\EvalConPractica');
+    }
+}
